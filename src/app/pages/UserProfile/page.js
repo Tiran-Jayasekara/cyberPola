137b2a502ac6ba08e7afa796125a795c42e23278
Tiran Jayasekara <70306643+Tiran2385@users.noreply.github.com>
2024-03-07 21:52:55 +0530

    update

--- src/app/pages/UserProfile/page.js
+++ src/app/pages/UserProfile/page.js
@@ -14,7 +14,7 @@ const UserProfile = () => {
 
     useEffect(() => {
         getUserData();
-    }, [])
+    })
 
     const getUserData = async () => {
         if (userData) {
@@ -34,7 +34,7 @@ const UserProfile = () => {
 
                 <h1 className='text-green-900 playfair-font text-center md:text-4xl text-2xl font-bold mx-10 md:mt-20 mt-10 mb-10'>User profile</h1>
 
-                {user ? <>
+                {user ? <div className='flex w-full flex-col'>
                     {/* <Image width={100} height={100} className='mx-auto rounded-full w-40 h-40' src='/assests/profile.jpeg' alt='' /> */}
                     <p className='playfair-font text-center mt-10'>{user.firstName} {user.lastName}</p>
                     <p className='playfair-font text-center mt-4'>{user.email}</p>
@@ -42,17 +42,17 @@ const UserProfile = () => {
                     <p className='playfair-font text-center mt-4'>{user.province}</p>
                     <p className='playfair-font text-center mt-4'>{user.town}</p>
                     <p className='playfair-font text-center mt-4'>{user.zipCode}</p>
-                </>
+                </div>
                     : ""}
 
             </div>
 
-            {user ? <div className='flex flex-row mx-auto justify-center items-center mt-20 '>
+            {/* {user ? <div className='flex flex-row mx-auto justify-center items-center mt-20 '>
                 <button onClick={() => { router.push('/pages/shops') }} className='bg-green-600 mx-4 p-2 rounded-2xl text-white playfair-font px-4'>Visit Shops</button>
                 <button onClick={() => { router.push('/pages/ShoppingCenter') }} className='bg-green-600 mx-4 p-2 rounded-2xl playfair-font text-white px-4'>Shopping Center</button>
             </div> : ""}
 
-            <Footer />
+            <Footer /> */}
         </>
     )
 }
