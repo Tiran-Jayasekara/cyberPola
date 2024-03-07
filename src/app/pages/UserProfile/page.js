1581ddf844e711a9e9c429b9a3a5dae49cae6721
Tiran Jayasekara <70306643+Tiran2385@users.noreply.github.com>
2024-03-07 21:10:11 +0530

    upload

--- src/app/pages/UserProfile/page.js
+++ src/app/pages/UserProfile/page.js
@@ -35,7 +35,7 @@ const UserProfile = () => {
                 <h1 className='text-green-900 playfair-font text-center md:text-4xl text-2xl font-bold mx-10 md:mt-20 mt-10 mb-10'>User profile</h1>
 
                 {user ? <>
-                    <Image width={100} height={100} className='mx-auto rounded-full w-40 h-40' src='/assests/profile.jpeg' alt='' />
+                    {/* <Image width={100} height={100} className='mx-auto rounded-full w-40 h-40' src='/assests/profile.jpeg' alt='' /> */}
                     <p className='playfair-font text-center mt-10'>{user.firstName} {user.lastName}</p>
                     <p className='playfair-font text-center mt-4'>{user.email}</p>
                     <p className='playfair-font text-center mt-4'>{user.mobile}</p>
@@ -43,9 +43,7 @@ const UserProfile = () => {
                     <p className='playfair-font text-center mt-4'>{user.town}</p>
                     <p className='playfair-font text-center mt-4'>{user.zipCode}</p>
                 </>
-                    : <>
-                        <button onClick={() => { router.push('/pages/UserLogin') }} className='playfair-font bg-green-600 rounded-2xl p-2 mb-20 text-white text-center md:text-2xl text-xl font-bold mx-10 md:mt-20 mt-10'>Please Logging</button>
-                    </>}
+                    : ""}
 
             </div>
 
